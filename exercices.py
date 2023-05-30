7cae4d565ac369139e8f096eb828bcc255b12a34
Pierre Verdiere <pverdiere59@gmail.com>
2023-05-30 11:19:14 +0200

    feat: exercices turtle

--- exercices.py
+++ exercices.py
@@ -1,1 +1,62 @@
-print("Hello world")
+import turtle
+
+turtle.speed(8)
+
+# The square
+turtle.penup()
+turtle.goto(-900,0)
+turtle.pendown()
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.penup()
+turtle.left(90)
+
+# pentagone
+
+turtle.goto(-700,0)
+turtle.pendown()
+turtle.forward(100)
+turtle.left(72)
+turtle.forward(100)
+turtle.left(72)
+turtle.forward(100)
+turtle.left(72)
+turtle.forward(100)
+turtle.left(72)
+turtle.forward(100)
+turtle.left(72)
+turtle.penup()
+
+# ship
+
+turtle.goto(-500,0)
+turtle.pendown()
+turtle.forward(200)
+turtle.left(180 - 60)
+turtle.forward(200)
+turtle.left(180 - 60)
+turtle.forward(200)
+turtle.left(180 - 60)
+turtle.penup()
+turtle.forward(100)
+turtle.left(90)
+turtle.pendown()
+turtle.forward(172)
+turtle.right(90)
+turtle.penup()
+turtle.goto(-500,-5)
+turtle.pendown()
+turtle.forward(200)
+turtle.right(180 - 45)
+turtle.forward(50)
+turtle.right(45)
+turtle.forward(130)
+turtle.right(45)
+turtle.forward(50)
+
+turtle.done()
